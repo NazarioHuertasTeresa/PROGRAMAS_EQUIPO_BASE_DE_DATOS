520eaec0093e99542d2305d011345a2bcbf0756b
Teresa Nazario Huertas <71741765+NazarioHuertasTeresa@users.noreply.github.com>
2020-09-23 13:39:48 -0500

    Update CONDICIONALES.cs

--- CONDICIONALES.cs
+++ CONDICIONALES.cs
@@ -6,21 +6,21 @@ namespace CONDICIONALES
     {
         static void Main(string[] args)
         {
-            int edad;
-            Console.WriteLine("Ingrese su edad: ");
+            int edad; 
+            Console.WriteLine("Por favor, Ingrese su edad");
+            int edad; 
             edad = int.Parse(Console.ReadLine());
-            if (edad >= 18)
+            if (edad)
+            Console.WriteLine("Ingrese su edad: ") 
+            if edad >=18)
             {
-
-                Console.WriteLine(" REGISTRO EXITOSO USTED ES MAYOR DE EDAD");
+                Console.WriteLine(" USTED ES MAYOR DE EDAD ¡SU REGISTRO HA SIDO EXITOSO!");
             }
             else
-            {
-                Console.WriteLine("REGISTRO NO EXITOSO, USTED ES MENOR DE EDAD ");
+            { 
+                Console.WriteLine("USTED ES MENOR DE EDAD ¡REGISTRO NO EXITOSO!")
             }
-            Console.ReadLine();         
-            
-        
+            Console.ReadLine();
         }
     }
 }
