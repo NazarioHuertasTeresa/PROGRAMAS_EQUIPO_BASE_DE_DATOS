f319c2157f8d98174ab90d7df5e05fda2fa97042
Teresa Nazario Huertas <71741765+NazarioHuertasTeresa@users.noreply.github.com>
2020-09-23 13:10:41 -0500

    Add files via upload

---
+++ CONDICIONALES.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CONDICIONALES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("Ingrese su edad: ");
+            edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+
+                Console.WriteLine(" REGISTRO EXITOSO USTED ES MAYOR DE EDAD");
+            }
+            else
+            {
+                Console.WriteLine("REGISTRO NO EXITOSO, USTED ES MENOR DE EDAD ");
+            }
+            Console.ReadLine();         
+            
+        
+        }
+    }
+}
