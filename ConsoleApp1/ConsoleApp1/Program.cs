8452e9d6c9a658e29fb84fb93cb41d0d67ac685b
DavidIrineo <ldavid7pyb@gmail.com>
2020-09-24 19:21:50 -0500

    Nuevo Proyecto
    se creo otro programa

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("Por favor, Ingrese su edad");
+            int edad;
+            edad = int.Parse(Console.ReadLine());
+            if (edad)
+                Console.WriteLine("Ingrese su edad: ")
+            if edad >= 18)
+            {
+                Console.WriteLine(" USTED ES MAYOR DE EDAD ¡SU REGISTRO HA SIDO EXITOSO!");
+            }
+            else
+            {
+                Console.WriteLine("USTED ES MENOR DE EDAD ¡REGISTRO NO EXITOSO!")
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
